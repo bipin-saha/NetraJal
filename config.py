1f23498f4ee883ae4b0050cbe8e1bb22303ff70b
Bipin Saha <47634524+bipin-saha@users.noreply.github.com>
2019-05-31 11:00:38 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # YouTube Video: https://www.youtube.com/watch?v=mP_Ln-Z9-XY
-EMAIL_ADDRESS = "phasorcomplex@gmail.com"
-SEND_ADDRESS = "jamarie.calil@cowstore.net"
-PASSWORD = "4YT38*k_"
+EMAIL_ADDRESS = "YOUR EMAIL"
+SEND_ADDRESS = "RECIPENT EMAIL"
+PASSWORD = "PASSWORD"
